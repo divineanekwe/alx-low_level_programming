a6a7a63c620faf6115cb67b5c0f72073a5029f79
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-08 22:47:35 +0000

    Print exactly "Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Program starts with the entry point
+ * puts - Write a line or string to the output(stdout) stream
+ *
+ * Description: Print exactly "Programming is like building a
+ * multilingual puzzle
+ * Return: Program should end with the value 0 (ends the program)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
