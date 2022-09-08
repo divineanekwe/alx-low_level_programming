5e1ba1d2a8f583006bd9380cf12873120827237b
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-08 23:03:20 +0000

    Print exactly with proper grammar, but the outcome is a piece of art,

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program starts with the entry point
+ * printf - Display formatted output to stdout
+ *
+ * Description: Print exactly with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: Program should return 0 (ends the program)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
