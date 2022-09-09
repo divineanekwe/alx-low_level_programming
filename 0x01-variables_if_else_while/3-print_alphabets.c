092c90e2fdf6f47efd90961de608d37eb40c27ae
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-09 23:50:29 +0000

    Print the alphabet in lowercase, and then in uppercase, followed by a new line. You can only use putchar() function

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Program starts with entry point
+ *
+ * putchar - Write a character, of unsigned
+ * char type, to standard output
+ *
+ * Description - Print the alphabet in lowercase,
+ * and then in uppercase, followed by a new line.
+ * You can only use the putchar function
+ *
+ * Return: Always return 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
