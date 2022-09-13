377d5466b7a19ae898297d1e3cfda14f0fbb0497
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-13 21:27:00 +0000

    Print _putchar, folled by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - Program starts with entry point
+  *
+  * printf - Display result to standard output
+  *
+  * Description - Print _putchar followed by new line
+  * The program should should return 0
+  *
+  * Return: Always return 0 (success)
+  */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
