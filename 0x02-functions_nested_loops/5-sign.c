72d3ea960c1578a17e5f4c1391c304f6233dfdc5
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-14 00:46:04 +0000

    Hot Fix: Print the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,6 +3,10 @@
  * print_sign - Prints the sign of a number
  * @n: Number to checked
  *
+ * Description - Prints '+' if @n is greater than zero,
+ * prints '0' if @n is zero,
+ * prints '-' if @n is less than zero
+ *
  * Return:  Always return 1 if @n is greater than zero,
  * or return 0 if @n is zero,
  * or return -1 if @n is less than zero
@@ -10,9 +14,12 @@
 int print_sign(int n)
 {
 	if (n > 0)
+		_putchar('+');
 		return (1);
 	else if (n == 0)
+		_putchar(48);
 		return (0);
 	else if (n < 0)
-		return (-1);
+		_putchar('-');
+	return (-1);
 }
