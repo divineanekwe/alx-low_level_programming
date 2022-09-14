dd9ea71d83a1ed09d43449c1aa4e4115b4d48fad
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-14 01:02:41 +0000

    Print the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - Print the last digit
+ * of a number
+ * @n: Number to be checked
+ *
+ * Return: Always return the value of the
+ * last digit
+ */
+int print_last_digit(int n)
+{
+	int rem = n % 10;
+
+	return (rem);
+}
