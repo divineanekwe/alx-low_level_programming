94e78ecba693383c423bb7cc74b948bf65a06e97
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-14 01:11:27 +0000

    Bug Fix: Print the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,11 +12,11 @@ int print_last_digit(int n)
 	int rem;
 
 	if (n < 0)
-		n = -1 * n;
+		n *= -1;
 	rem = n % 10;
 
 	if (rem < 0)
-		rem = -1 * a;
+		rem *= -1;
 	_putchar(rem + '0');
 	return (rem);
 }
