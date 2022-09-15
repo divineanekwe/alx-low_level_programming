d409580c473a92c109062b23a453e01b1bf4e7ac
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-15 18:46:02 +0000

    Find and print the largest prime factor of 612852475143

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+  * main - find and print the largest prime
+  * factor of the number 612852475143
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	long int i, n, lpf;
+
+	n = 612852475143;
+
+	while (n % 2 == 0)
+	{
+		n /= 2;
+		lpf = 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			n /= i;
+			lpf = i;
+		}
+	}
+
+
+	printf("%ld\n", lpf);
+
+	return (0);
+}
