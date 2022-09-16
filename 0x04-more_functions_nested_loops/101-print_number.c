ea87103d48f398a3e03429a6e8818d0c34364118
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-16 02:29:19 +0000

    Hot fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,16 +6,21 @@
  */
 void print_number(int n)
 {
+	unsigned int n2;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
-	}
-
-	if (n / 10)
+		n2 = -n;
+	} else
 	{
-		print_number(n / 10);
+		n2 = n;
 	}
 
-	_putchar((n % 10) + '0');
+	if (n2 / 10)
+	{
+		print_number(n2 / 10);
+	}
+
+	_putchar((n2 % 10) + '0');
 }
