6f6b0b2771ff2b9a9b302534410b563d79125b96
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-19 20:43:02 +0000

    Update the value of a pointer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - updates a pointed value to 98
+  * @n: pointer of value to be updated
+  *
+  * Return: void (nothing)
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
