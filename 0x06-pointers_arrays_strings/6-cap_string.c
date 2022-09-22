406bf24b4d01a9c3ab8e4e6c7998e51d640fd16e
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-22 15:44:40 +0000

    Remove tabulation

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -21,8 +21,12 @@ char *cap_string(char *s)
 		/* 9 is horinzontal tab (TAB) and 11 is vertical tab (VT) */
 		if (c == ',' || c == ';' || c == '.' || c == '!' || c == '?' ||
 			c == '"' || c == '(' || c == ')' || c == '{' ||
-			c == '}' || c == 10 || c == ' ' || c == 9)
+			c == '}' || c == 10 || c == ' ' || c == 9 || c == 11)
 		{
+			if (c == 9 || c == 11)
+			{
+				*(s + i) = ' ';
+			}
 			if (*(s + i + 1) >= 'a' && *(s + i + 1) <= 'z')
 			{
 				*(s + i + 1) -= 32;
