999eeac1debf398bcfdde1dc4f0ea1c5d30bbaf7
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-22 02:07:47 +0000

    Capitalize all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * cap_string - capitalizes all words of a string
+  * @s: String to capitalize
+  *
+  * Description - Separators of words: space,
+  * tabulation, new line, ',', ';', '.', '!',
+  * '?', '"', '(', ')', '{', '}'
+  *
+  * Return: A pointer to @s
+  */
+char *cap_string(char *s)
+{
+	int i = 0;
+	char c;
+
+	while (*(s + i) != '\0')
+	{
+		c = *(s + i);
+		/* 9 is horinzontal tab (TAB) and 11 is vertical tab (VT) */
+		if (c == ',' || c == ';' || c == '.' || c == '!' || c == '?' ||
+			c == '"' || c == '(' || c == ')' || c == '{' ||
+			c == '}' || c == '\n' || c == ' ' || c == 9 || c == 11)
+		{
+			if (*(s + i + 1) >= 'a' && *(s + i + 1) <= 'z')
+			{
+				*(s + i + 1) -= 32;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
