bb782414e96e5bef3191b211768b18c23980ee31
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-22 19:37:45 +0000

    Fix logic flow

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,16 +8,19 @@
   */
 void print_number(int n)
 {
+	unsigned int un;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
+	un = n;
 
-	if (n / 10)
+	if (un / 10)
 	{
-		print_number(n / 10);
+		print_number(un / 10);
 	}
 
-	_putchar((n % 10) + '0');
+	_putchar((un % 10) + '0');
 }
