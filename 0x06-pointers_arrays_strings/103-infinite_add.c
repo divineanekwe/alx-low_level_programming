b47c7fa857a4b539850ce919e7e318b928c3b7ae
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-23 00:14:09 +0000

    Fix logic flow

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,49 +1,83 @@
 #include "main.h"
-#include <stdio.h>
+
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 /**
- * infinite_add - adds two numbers
+ * add_strings - adds two numbers stored in string data type
  * @n1: First number to add
  * @n2: Second number to add
- * @r: Buffer to store result
- * @size_r: Size of buffer @r
+ * @r: Buffer to store the result.
+ * @r_index: Current index of the buffer.
  *
- * Return: A pointer, if @r can store result
- * 0 is returned, if @r cannot store result
+ * Return: A pointer to @r, if @r can store result
+ * 0, if @r cannot store result
  */
+
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+{
+	int num, tens = 0;
+
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n1; n1--, r_index--)
+	{
+		num = (*n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n2; n2--, r_index--)
+	{
+		num = (*n2 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	if (tens && r_index >= 0)
+	{
+		*(r + r_index) = (tens % 10) + '0';
+		return (r + r_index);
+	}
+
+	else if (tens && r_index < 0)
+		return (0);
+
+	return (r + r_index + 1);
+}
+/**
+ * infinite_add - adds two numbers.
+ * @n1: First number to add
+ * @n2: Second number to add
+ * @r: Buffer to store the result
+ * @size_r: Size of Buffer
+ *
+ * Return: A pointer to @r, if @r can store result
+ * 0 if @r cannot store result
+ */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, diff, big, small, cyc, cyc2, count, tmp, ten, check;
-	char *less, *more;
+	int index, n1_len = 0, n2_len = 0;
 
-	i = 0, j = 0;
-	while (n1[i] != '\0')
-		i++;
-	while (n2[j] != '\0')
-		j++;
-	if (i >= j)
-		big = i, small = j, diff = i - j + 1, less = n2, more = n1;
-	else
-		big = j, small = i, diff = j - i + 1, less = n1, more = n2;
-	count = 0, cyc = diff, ten = 0;
-	for (count = 0; count < small; count++, cyc++)
-		r[cyc] = less[count];
-	for (cyc2 = big; cyc2 >= 1; cyc2--)
-	{
-		if (cyc > size_r - 1)
-			return (0);
-		if (r[cyc2] <= 57 && r[cyc2] >= 48)
-			tmp = (r[cyc2] - '0') + (more[cyc2 - 1] - '0') + ten;
-		else
-			tmp = (more[cyc2 - 1] - '0') + ten;
-		ten = tmp / 10;
-		tmp = tmp - (ten * 10);
-		r[cyc2] = '0' + tmp;
-	}
-	r[cyc2] = '0' + ten;
-	if (ten == 0)
-		r++;
-	for (check = 0; check < size_r; check++)
-		if (r[check] > 57 || r[check] < 48)
-			r[check] = 0;
-	return (r);
+	for (index = 0; *(n1 + index); index++)
+		n1_len++;
+
+	for (index = 0; *(n2 + index); index++)
+		n2_len++;
+
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
+		return (0);
+
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
+	*(r + size_r) = '\0';
+
+	return (add_strings(n1, n2, r, --size_r));
 }
