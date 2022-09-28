1c4979591ccdf4687c9e65155a554d0a394e54b4
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-28 22:59:38 +0000

    Print a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _print_rev_recursion - prints a string in reverse
+  * @s: String to print
+  *
+  * Return: Void (nothing)
+  */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		_print_rev_recursion(s);
+		s--;
+		_putchar(*s);
+	}
+}
