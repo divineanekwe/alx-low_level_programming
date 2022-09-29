5deaf262c25bd556999dc3399c8e3d68d015ffa9
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-29 02:06:48 +0000

    Add parameter type for function prototype: palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,6 +13,6 @@ int is_prime_number(int n);
 int is_prime(int n, int i);
 int is_palindrome(char *s);
 int _strlen(char *s);
-int palindrome(s, i, slen);
+int palindrome(char *s, int i, int slen);
 
 #endif /* MAIN_H */
