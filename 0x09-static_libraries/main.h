a2a2878f7f0ac0158e888968054aba1f6e3b6ebb
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-30 21:13:49 +0000

    Modify function prototype: *_memset

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -15,6 +15,6 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-char *_memset(char *s, char *b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif /* MAIN_H */
