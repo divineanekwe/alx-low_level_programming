d7ce5cbd0ee636625a2afa8a94834d8d03ea0a90
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-30 22:13:25 +0000

    Locate a substring in a string

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+  * _strstr - locates a substring
+  * @haystack: String to be checked
+  * @needle: Substring to search for
+  *
+  * Return: A pointer to the beginning
+  * of the located substring.
+  * Otherwise, return NULL if @needle
+  * is not found
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+
+	if (*(needle + j) == '\0')
+	{
+		return (haystack);
+	}
+
+	while (*(haystack + i) != '\0')
+	{
+		for ( ; *(needle + j) == *(haystack + i); j++)
+		{
+			if (*(needle + j + 1) == '\0')
+			{
+				return (haystack + i - j);
+			}
+			i++;
+		}
+		i++;
+	}
+	return ('\0');
+}
