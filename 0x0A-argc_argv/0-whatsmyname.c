6095c049c928268e3e9bf8e088d78f2cd1b33c01
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-30 23:56:13 +0000

    Print the name of it's program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * main - prints the name of the program
+  * @argc: Argument count
+  * @argv: Argument vector
+  *
+  * Return: Always 0
+  */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", *argv);
+	return (0);
+}
