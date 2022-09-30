53e26f465bcbf33a0dd8355fd2a5c47b55efe5e3
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-09-30 18:49:43 +0000

    Get the length of a string

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _strlen - gets the length of a string
+  * @s: String to be checked
+  *
+  * Return: Length of string @s
+  */
+int _strlen(char *s)
+{
+	if (*s != '\0')
+		return(1 + _strlen(s + 1));
+	return (0);
+}
