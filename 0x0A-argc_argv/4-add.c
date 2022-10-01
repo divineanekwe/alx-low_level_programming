c45db43cb68aee682bba894ea7beab3c2ebfe08d
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-10-01 00:48:30 +0000

    Bug Fix: Add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,15 +18,18 @@ int main(int argc, char **argv)
 	int i;
 	int sum = 0;
 
-	for (i = 1; i < argc; i++)
+	if (argc > 1)
 	{
-		if (*(*(argv + i) + 0) > 48 && *(*(argv + i) + 0) < 58)
+		for (i = 1; i < argc; i++)
 		{
-			sum += atoi(argv[i]);
-		} else
-		{
-			printf("Error\n");
-			return (1);
+			if (*(*(argv + i) + 0) >= 48 && *(*(argv + i) + 0) < 58)
+			{
+				sum += atoi(argv[i]);
+			} else
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 	}
 	printf("%d\n", sum);
