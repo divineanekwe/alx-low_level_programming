c3af1507efb68f0d5ba841b671c4bebbe7586458
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-10-01 00:59:33 +0000

    Debug: Add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,20 +18,17 @@ int main(int argc, char **argv)
 	int i, j;
 	int sum = 0;
 
-	if (argc > 1)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; *(*(argv + i) + j) != '\0'; j++)
 		{
-			for (j = 0; *(*(argv + i) + j) != '\0'; j++)
+			if (*(*(argv + i) + j) >= 48 && *(*(argv + i) + j) < 58)
 			{
-				if (*(*(argv + i) + j) >= 48 && *(*(argv + i) + j) < 58)
-				{
-					sum += atoi(argv[i]);
-				} else
-				{
-					printf("Error\n");
-					return (1);
-				}
+				sum += atoi(argv[i]);
+			} else
+			{
+				printf("Error\n");
+				return (1);
 			}
 		}
 	}
