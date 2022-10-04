77688bb2af324817e47187ce71b1119316faeac3
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-10-04 18:30:14 +0000

    Debug

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,15 +24,15 @@ char *str_concat(char *s1, char *s2)
 	s = (char *)malloc(i + j + 1);
 	if (s == NULL)
 		return ('\0');
-	if (*s1 == '\0' && *s2 == '\0')
+	if ((*s1 == '\0' && *s2 == '\0') || (s1 == 0x00 && s2 == 0x00))
 	{
 		*s = '\0';
-	} else if (*s1 == '\0')
+	} else if (*s1 == '\0' || s1 == 0x00)
 	{
 		for (i = 0; s2[i] != '\0'; i++)
 			s[i] = s2[i];
 		s[i] = '\0';
-	} else if (*s2 == '\0')
+	} else if (*s2 == '\0' || s2 == 0x00)
 	{
 		for (i = 0; s1[i] != '\0'; i++)
 			s[i] = s1[i];
