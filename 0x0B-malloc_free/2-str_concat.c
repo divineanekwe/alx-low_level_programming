07f036fcdfc06e1dec5ed748decbe3d2b50c5f48
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-10-04 23:49:35 +0000

    Final Debug

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,38 +15,27 @@ char *str_concat(char *s1, char *s2)
 {
 	int i = 0;
 	int j = 0;
+	int a;
 	char *s;
 
-	while (s1[i] != '\0')
-		i++;
-	while (s2[j] != '\0')
-		j++;
-	s = (char *)malloc(i + j + 1);
-	if (s == NULL)
-		return ('\0');
-	if ((*s1 == '\0' && *s2 == '\0') || (s1 == 0x00 && s2 == 0x00))
+	while (s1 != 0x00 && *s1 != '\0' && s1[i] != '\0')
 	{
-		*s = '\0';
-	} else if (*s1 == '\0' || s1 == 0x00)
-	{
-		for (i = 0; s2[i] != '\0'; i++)
-			s[i] = s2[i];
-		s[i] = '\0';
-	} else if (*s2 == '\0' || s2 == 0x00)
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-			s[i] = s1[i];
-		s[i] = '\0';
-	} else
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-			s[i] = s1[i];
-		for (j = 0; s2[j] != '\0'; j++)
-		{
-			s[i] = s2[j];
 			i++;
-		}
-		s[i] = '\0';
 	}
+	while (s2 != 0x00 && *s2 != '\0' && s2[j] != '\0')
+	{
+			j++;
+	}
+	s = (char *)malloc(i + j + 1);
+	for (a = 0; i != 0 && a < i; a++)
+	{
+			s[a] = s1[a];
+	}
+	for (a = 0; j != 0 && a < j; a++)
+	{
+		s[i] = s2[a];
+		i++;
+	}
+	s[i] = '\0';
 	return (s);
 }
