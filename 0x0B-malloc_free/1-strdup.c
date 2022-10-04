098a4764ff85ca84f3ed3300992a1b0f395cbec5
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-10-04 17:33:01 +0000

    Debug

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,11 +16,13 @@ char *_strdup(char *str)
 	int i, j;
 	char *s;
 
+	if (str == 0x00)
+		return ('\0');
 	i = 0;
 	while (str[i] != '\0')
 		i++;
 	s = (char *)malloc(i);
-	if (*str == '\0' || s == NULL)
+	if (s == NULL)
 		return ('\0');
 	for (j = 0; j < i; j++)
 		s[j] = str[j];
