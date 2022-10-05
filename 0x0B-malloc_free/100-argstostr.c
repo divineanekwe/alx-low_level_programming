667d7e4b90f92b7d150afdc12eada0b3e4d76f3d
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-10-05 02:39:07 +0000

    Concatenate all the arguments of your program

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+  * argstostr - concatenates all the arguments
+  * of your program
+  * @ac: Argument count
+  * @av: Argument vector
+  *
+  * Description - each argument should be followed
+  * by a '\n' in the new string
+  *
+  * Return: A pointer to the new string.
+  * Otherwise, return NULL if it fails.
+  * Return NULL if ac == 0 or av == NULL
+  */
+char *argstostr(int ac, char **av)
+{
+	int i, j;
+	int slen = 0;
+	char *s, *sadd;
+
+	if (ac < 1 || av == NULL)
+	{
+		return ('\0');
+	}
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (*(*(av + i) + j) != '\0')
+		{
+			slen++;
+			j++;
+		}
+		slen++;
+	}
+	s = (char *)malloc(slen + 1);
+	sadd = s;
+	if (s == NULL)
+	{
+		return ('\0');
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			*sadd = av[i][j];
+			sadd++;
+		}
+		*sadd = '\n';
+		sadd++;
+	}
+	*sadd = '\0';
+	return (s);
+}
