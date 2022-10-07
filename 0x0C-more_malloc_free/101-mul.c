96542db87a459f36b7b81963f0dde4aeeeb82baa
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-10-06 20:13:07 -0700

    Create 101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,114 @@
+#include "main.h"
+
+/**
+ * is_digit - checks if a string is numeric
+ * @s: The string to check
+ *
+ * Return: 0 if string is not numeric.
+ * Otherwise, return 1
+ */
+int is_digit(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+		{
+			return (0);
+		}
+		++i;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: String to get the length
+ *
+ * Return: Length of string @s
+ */
+int _strlen(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen(s + 1));
+	}
+	return (0);
+}
+
+/**
+ * error - prints Error with exit status of 98
+ */
+void error(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	char *s1, *s2;
+	int i, carry,  len1, len2, slen, digit1, digit2, *result;
+	int a = 0;
+
+	s1 = argv[1];
+	s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+	{
+		error();
+	}
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	slen = len1 + len2 + 1;
+	result = malloc(sizeof(int) * slen);
+	if (!result)
+	{
+		return (1);
+	}
+	for (i = 0; i <= (len1 + len2); i++)
+	{
+		result[i] = 0;
+	}
+	for (--len1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2--; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+		{
+			result[len1 + len2 + 1] += carry;
+		}
+	}
+	for (i = 0; i < (slen - 1); ++i)
+	{
+		if (result[i])
+		{
+			a = 1;
+		}
+		if (a)
+		{
+			_putchar(result[i] + '0');
+		}
+	}
+	if (!a)
+	{
+		_putchar('0');
+	}
+	_putchar('\n');
+	free(result);
+	return (0);
+}
