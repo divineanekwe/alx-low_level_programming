9cac81a7ec168e54dc48774aaa052911a6e5b8d0
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-10-11 00:01:36 +0000

    Create A New Type: struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - defines new type dog
+  * @name: Pointer to dog's name
+  * @age: Age of dog
+  * @owner: Pointer to dog's owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
