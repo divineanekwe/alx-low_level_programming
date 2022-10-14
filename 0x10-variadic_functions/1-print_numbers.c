d333d2450b44da253355c82c985cc9c4b8e5d910
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-10-14 00:37:44 +0000

    Fix logic flow

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,18 +17,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		if (i == (n - 1))
+		if (!separator)
 		{
-			printf("%d\n", va_arg(ap, int));
+			printf("%d", va_arg(ap, int));
+		} else if (separator && i == 0)
+		{
+			printf("%d", va_arg(ap, int));
 		} else
 		{
-			if (separator == NULL)
-			{
-				printf("%d", va_arg(ap, int));
-			} else
-			{
-				printf("%d%s", va_arg(ap, int), separator);
-			}
+			printf("%s%d", separator, va_arg(ap, int));
 		}
 	}
+	va_end(ap);
+	printf("\n");
 }
