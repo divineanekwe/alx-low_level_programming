31b961368ac0bf3ba450f8a441f5c6075262bdfa
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-10-21 15:02:02 +0000

    Print a message before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+  * before_main - prints message before
+  * the main function is executed
+  *
+  * Return: Void (nothing)
+  */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
