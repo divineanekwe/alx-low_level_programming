e93400450d5b72006195b6fdfe3c5e074280ec0b
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-10-30 20:42:21 +0000

    Debug

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,19 +14,17 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 	int i = 0;
 
-	if (b == NULL || b[i] == '\0')
+	if (!b)
 	{
 		return (0);
 	}
-	while (b[i] == '0' || b[i] == '1')
+	for ( ; b[i]; i++)
 	{
-		num <<= 1;
-		num += b[i] - '0';
-		i++;
-	}
-	if (b[--i] != '0' || b[--i] != '1')
-	{
-		return (0);
+		if (b[i] < '0' || b[i] > '1')
+		{
+			return (0);
+		}
+		num = 2 * num + (b[i] - '0');
 	}
 	return (num);
 }
