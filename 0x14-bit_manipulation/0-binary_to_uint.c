d1dbe7091791aad1f0f449795c1e23f2c6e0eb07
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-10-30 20:39:26 +0000

    Fix logic flow

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,13 +14,19 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 	int i = 0;
 
-	if (b[i] == '\0')
+	if (b == NULL || b[i] == '\0')
+	{
 		return (0);
+	}
 	while (b[i] == '0' || b[i] == '1')
 	{
 		num <<= 1;
 		num += b[i] - '0';
 		i++;
+	}
+	if (b[--i] != '0' || b[--i] != '1')
+	{
+		return (0);
 	}
 	return (num);
 }
