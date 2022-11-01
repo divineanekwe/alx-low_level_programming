2b6a98401a57a2765be47be65e8f124f3b2b93be
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-11-01 03:40:05 +0000

    Add function prototype: check_elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -20,5 +20,6 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 char *create_buffer(char *filename);
 void close_file(int fd);
+void check_elf(unsigned char *e_ident);
 
 #endif /* MAIN_H */
