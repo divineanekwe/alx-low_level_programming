35b391efba8db4c63f39e4b4a3bd13aa49fa0d93
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-11-01 03:42:49 +0000

    Add function prototype: print_abi

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -25,5 +25,6 @@ void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
 void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
 
 #endif /* MAIN_H */
