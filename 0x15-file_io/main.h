1aaceac77943d3c6f52899958e4acb6f1aa5bdf5
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-11-01 03:45:29 +0000

    Add function prototype: close_elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -29,5 +29,6 @@ void print_abi(unsigned char *e_ident);
 void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 #endif /* MAIN_H */
